4364dd404cff64f0ac4234ef7ef48d9d9cefeb6b
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-22 20:51:14 +0400

    Some refactors

--- turingLessons/src/tasks/MultiplicationApp.java
+++ turingLessons/src/tasks/MultiplicationApp.java
@@ -2,7 +2,6 @@ package tasks;
 
 public class MultiplicationApp {
     public static void main(String[] args){
-
         for (int i = 0; i <= 10; i++) {
             System.out.println(2 * i);
         }

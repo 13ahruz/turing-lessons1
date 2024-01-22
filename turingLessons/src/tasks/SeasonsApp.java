e2fb44432f1d21795190a5daaae97e1a450f6aad
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-22 20:06:13 +0400

    Some refactors

--- turingLessons/src/tasks/SeasonsApp.java
+++ turingLessons/src/tasks/SeasonsApp.java
@@ -9,7 +9,7 @@ public class SeasonsApp {
         System.out.println("Input ur month number: ");
         int a = sc.nextInt();
 
-        String  monthName = (a == 1 || a == 2 || a == 12) ? "Your month is in winter."
+        String monthName = (a == 1 || a == 2 || a == 12) ? "Your month is in winter."
                 : (a == 3 || a == 4 || a == 5) ? "Your month is in spring." : (a == 6 || a == 7 || a == 8) ? "Your month is in autumn."
                 : "Your month is in summer.";
 

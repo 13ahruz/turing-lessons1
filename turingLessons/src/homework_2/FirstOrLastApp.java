afcf61423c33c3d2c3d309f00f1ed3dbb207bdcb
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-23 00:05:37 +0400

    Some refactors

--- turingLessons/src/homework_2/FirstOrLastApp.java
+++ turingLessons/src/homework_2/FirstOrLastApp.java
@@ -6,12 +6,21 @@ public class FirstOrLastApp {
     public static void main(String[] args) {
         int num = new Scanner(System.in).nextInt();
         int largestDigit = 0;
+        boolean isEqual = false;
         while (num > 0) {
             int digit = num % 10;
-            largestDigit = (largestDigit > digit) ? largestDigit : digit; //largestDigit = Math.max(largestDigit, digit);
+            if (digit == largestDigit) {
+                isEqual = true;
+            } else if (digit > largestDigit) {
+                largestDigit = digit;
+            }
             num /= 10;
         }
-        System.out.println(largestDigit);
+        if (isEqual) {
+            System.out.println("=");
+        } else {
+            System.out.println(largestDigit);
+        }
     }
 }
 

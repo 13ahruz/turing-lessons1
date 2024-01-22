347ad1ed676002533853c0e1ea8462251e8b0f45
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-22 19:32:47 +0400

    Divisor and reminder, simple java code

---
+++ Turing Lesoon/src/dividend.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class dividend {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input ur first number: ");
+        int a = sc.nextInt();
+
+        System.out.println("Input ur second number: ");
+        int b = sc.nextInt();
+
+
+
+        System.out.print("Reminder of your numbers is: " +  a / b);
+
+    }
+}

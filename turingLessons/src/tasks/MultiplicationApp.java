6a0953d53ea0db0c13b57cf1bda44f1d0995f2bb
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-22 20:49:17 +0400

    Some refactors

---
+++ turingLessons/src/tasks/MultiplicationApp.java
@@ -0,0 +1,13 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class MultiplicationApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(2 * i);
+        }
+    }
+}

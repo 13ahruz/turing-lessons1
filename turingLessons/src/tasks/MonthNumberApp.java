5a24d72ca3d0d56c08e2733ed0d38218811d4754
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-22 20:13:50 +0400

    Some refactors

--- turingLessons/src/tasks/MonthNumberApp.java
+++ turingLessons/src/tasks/MonthNumberApp.java
@@ -9,43 +9,31 @@ public class MonthNumberApp {
         System.out.println("Input ur number: ");
         int monthNum = sc.nextInt();
 
-        if (monthNum == 1){
+        if (monthNum == 1) {
             System.out.println("Your month is January.");
-        }
-        else if (monthNum == 2){
+        } else if (monthNum == 2) {
             System.out.println("Your month is February.");
-        }
-        else if (monthNum == 3){
+        } else if (monthNum == 3) {
             System.out.println("Your month is March.");
-        }
-        else if (monthNum == 4){
+        } else if (monthNum == 4) {
             System.out.println("Your month is April.");
-        }
-        else if (monthNum == 5){
+        } else if (monthNum == 5) {
             System.out.println("Your month is May.");
-        }
-        else if (monthNum == 6){
+        } else if (monthNum == 6) {
             System.out.println("Your month is June.");
-        }
-        else if (monthNum == 7){
+        } else if (monthNum == 7) {
             System.out.println("Your month is July.");
-        }
-        else if (monthNum == 8){
+        } else if (monthNum == 8) {
             System.out.println("Your month is August.");
-        }
-        else if (monthNum == 9){
+        } else if (monthNum == 9) {
             System.out.println("Your month is September.");
-        }
-        else if (monthNum == 10){
+        } else if (monthNum == 10) {
             System.out.println("Your month is October.");
-        }
-        else if (monthNum == 11){
+        } else if (monthNum == 11) {
             System.out.println("Your month is November.");
-        }
-        else if (monthNum == 12){
+        } else if (monthNum == 12) {
             System.out.println("Your month is December.");
-        }
-        else {
+        } else {
             System.out.println("Not valid month!");
         }
 

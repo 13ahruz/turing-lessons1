53d3e4eeaff8653c615c9b7de56015f041ec1d55
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-22 23:18:19 +0400

    Some refactors

---
+++ turingLessons/src/homework_2/EducationalLevel.java
@@ -0,0 +1,19 @@
+package homework_2;
+
+import java.util.Scanner;
+
+public class EducationalLevel {
+    public static void main(String[] args) {
+        int grade = new Scanner(System.in).nextInt();
+
+        String level = "Initial";
+        if (grade >= 10) {
+            level = "High";
+        } else if (grade >= 7) {
+            level = "Sufficient";
+        } else if (grade >= 4) {
+            level = "Average";
+        }
+        System.out.println(level);
+    }
+}

3147194f9e42fcdc9c6ee028ddf4f4ade4eeef7c
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-26 20:19:01 +0400

    Some changes

--- turingLessons/src/tasks/NSizeArrayApp.java
+++ turingLessons/src/tasks/NSizeArrayApp.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class NSizeArrayApp {
     public static void main(String[] args) {
         int num = new Scanner(System.in).nextInt();
-        int counter = num;
+        int counter = 1;
 
         for (int i = 0; i < num; i++) {
-            for (int j = 0; j < counter; j++) {
+            for (int j = 1; j <= counter; j++) {
                 System.out.print("#");
             }
-            counter--;
+            counter++;
             System.out.println();
         }
     }
